1f4acd0378403bcedfff698affd1151ab78d8a9d
Bryan Santos <sp.bryansantos@gmail.com>
2021-08-22 15:29:02 -0300

    Aula 02 - Processamento e Casting

---
+++ curso-java/src/estrutura/sequencial/Processamento.java
@@ -0,0 +1,22 @@
+package estrutura.sequencial;
+
+public class Processamento {
+
+	public static void main(String[] args) {
+
+		int a, b, d;
+		double resultado, c;
+		
+		a = 5;
+		b = 2;
+		c = 5.0;
+		
+		// Casting
+		resultado = (double) a / b;
+		d = (int) c;
+		
+		System.out.println(resultado);
+		System.out.println(d);
+	}
+
+}
